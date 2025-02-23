69cd6fad7084fa40e558918cda455b04c225bddc
Tomas Kirnig <tomas.kirnig.st@vsb.cz>
2025-02-23 15:48:47 +0100

    Functional version

--- PJP_CV1/Program.cs
+++ PJP_CV1/Program.cs
@@ -1,54 +1,123 @@
 ﻿using System;
-using System.Collections.Generic;
 
-class Program
+public class Program
 {
-    private static int pos;
-    private static string expression;
-    private static List<int> results;
-
-    static void Main()
+    public static void Main()
     {
-        pos = 0;
-        Console.Write("Enter number of lines: ");
-        string count = Console.ReadLine();
+        Console.WriteLine("Enter number of expressions: ");
+        if (!int.TryParse(Console.ReadLine(), out int n))
+            return;
 
-        for (int i = Int32.Parse(count); i > 0; i--)
+        for (int i = 0; i < n; i++)
         {
-            expression = Console.ReadLine();
-            expression = expression.Replace(" ", "");
-            results.Add(Evaluate(expression));
+            Console.WriteLine($"Enter expression ({i+1}.): ");
+            string expr = Console.ReadLine();
+            Console.WriteLine(Parser.Evaluate(expr));
+        }
+    }
+}
+
+public static class Parser
+{
+    public static string Evaluate(string expression)
+    {
+        try
+        {
+            int result = ParseExpression(expression);
+            return result.ToString();
+        }
+        catch (Exception)
+        {
+            return "ERROR";
         }
     }
 
-    static int Evaluate(string str)
+    private static int ParseExpression(string s)
     {
-        int results = 0;
-        string tmp = "";
-        while (pos < expression.Length)
+        int pos = 0;
+        int result = ParseExpression(s, ref pos);
+        SkipWhitespace(s, ref pos);
+        if (pos < s.Length)
+            throw new Exception("Unexpected characters");
+        return result;
+    }
+
+    // ParseExpression: Expression = Term { ('+' | '-') Term }
+    private static int ParseExpression(string s, ref int pos)
+    {
+        int result = ParseTerm(s, ref pos);
+        SkipWhitespace(s, ref pos);
+        while (pos < s.Length && (s[pos] == '+' || s[pos] == '-'))
         {
-            if (str[pos] > '0' && str[pos] < '9')
-            {
-                tmp.Append(str[pos]);
-                pos++;
-            }
-            else if (str[pos] == '*' || str[pos] == '/')
-            {
-
-            }
-            else if (str[pos] == '+' || str[pos] == '-')
-            {
-
-            }
-            else if (str[pos] == '(')
-            {
-                Evaluate(str.Substring(pos, str.IndexOf(')') - pos);
-            }
-            else if (str[pos] == ')')
-            {
-                pos++;
-                return results;
-            }
+            char op = s[pos];
+            pos++; // consume operator
+            int term = ParseTerm(s, ref pos);
+            result = op == '+' ? result + term : result - term;
+            SkipWhitespace(s, ref pos);
         }
+        return result;
+    }
+
+    // ParseTerm: Term = Factor { ('*' | '/') Factor }
+    private static int ParseTerm(string s, ref int pos)
+    {
+        int result = ParseFactor(s, ref pos);
+        SkipWhitespace(s, ref pos);
+        while (pos < s.Length && (s[pos] == '*' || s[pos] == '/'))
+        {
+            char op = s[pos];
+            pos++; // consume operator
+            int factor = ParseFactor(s, ref pos);
+            if (op == '*')
+                result *= factor;
+            else
+            {
+                if (factor == 0)
+                    throw new Exception("Division by zero");
+                result /= factor;
+            }
+            SkipWhitespace(s, ref pos);
+        }
+        return result;
+    }
+
+    // ParseFactor: Factor = Number | '(' Expression ')'
+    private static int ParseFactor(string s, ref int pos)
+    {
+        SkipWhitespace(s, ref pos);
+        if (pos >= s.Length)
+            throw new Exception("Unexpected end of input");
+
+        if (s[pos] == '(')
+        {
+            pos++; // consume '('
+            int result = ParseExpression(s, ref pos);
+            SkipWhitespace(s, ref pos);
+            if (pos >= s.Length || s[pos] != ')')
+                throw new Exception("Missing closing parenthesis");
+            pos++; // consume ')'
+            return result;
+        }
+        else if (char.IsDigit(s[pos]))
+        {
+            int start = pos;
+            while (pos < s.Length && char.IsDigit(s[pos]))
+                pos++;
+            string numberStr = s.Substring(start, pos - start);
+            if (!int.TryParse(numberStr, out int value))
+                throw new Exception("Invalid number");
+            return value;
+        }
+        else
+        {
+            throw new Exception("Unexpected character: " + s[pos]);
+        }
+    }
+
+    // Helper method to skip whitespace in the expression.
+    private static void SkipWhitespace(string s, ref int pos)
+    {
+        while (pos < s.Length && char.IsWhiteSpace(s[pos]))
+            pos++;
     }
 }
--- PJP_CV1/obj/Debug/net8.0/PJP1.AssemblyInfo.cs
+++ PJP_CV1/obj/Debug/net8.0/PJP1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PJP1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee57d58ad307b26f8119a32fe6553146fc2f6de2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PJP1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PJP1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
